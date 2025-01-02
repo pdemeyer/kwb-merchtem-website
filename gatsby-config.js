e9d6e60937fcf48df3027831e3b83df0315f6dff
pdemeyer <pdemeyer@gmail.com>
2025-01-02 10:51:32 +0100

    restart

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,24 @@
 /**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
   siteMetadata: {
-    title: `KWB Merchtem`,
+    title: `Gatsby Starter Blog`,
     author: {
-      name: `Pauwel Demeyer`,
-      summary: `father, software architect, cook, baker, and lover of good food.`,
+      name: `Kyle Mathews`,
+      summary: `who lives and works in San Francisco building useful things.`,
     },
-    description: `De KWB Merchtem website.`,
+    description: `A starter blog demonstrating what Gatsby can do.`,
+    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `pdemeyer`,
+      twitter: `kylemathews`,
     },
-    siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -112,7 +118,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
