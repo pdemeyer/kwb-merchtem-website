ca61ad852cb7e39fc870ae906a89038dab0c70e2
pdemeyer <pdemeyer@gmail.com>
2025-01-03 20:44:42 +0100

    part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="kalender maand per maand"
+        src="../images/kalender-maand-per-maand.jpg"
+      />
     </Layout>
   )
 }
